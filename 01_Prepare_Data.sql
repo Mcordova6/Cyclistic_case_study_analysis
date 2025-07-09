f980e377e4b2766cd825f3d8dc1cab7689991b4b
Mcordova6 <mcordova6@miners.utep.edu>
2025-07-09 16:02:00 -0600

    01_Prepare_Data.sql

---
+++ 01_Prepare_Data.sql
@@ -0,0 +1,27 @@
+-- Created new BigQuery Table with unionized data
+CREATE TABLE `test-project-461721.cyclistic_bike_share_data.rides_12_months` AS
+
+-- Unionized all the datasets from June 2024 to May 2025
+SELECT * FROM `test-project-461721.cyclistic_bike_share_data.202406-divvy-tripdata`
+UNION ALL
+SELECT * FROM `test-project-461721.cyclistic_bike_share_data.202407-divvy-tripdata`
+UNION ALL
+SELECT * FROM `test-project-461721.cyclistic_bike_share_data.202408-divvy-tripdata`
+UNION ALL
+SELECT * FROM `test-project-461721.cyclistic_bike_share_data.202409-divvy-tripdata`
+UNION ALL
+SELECT * FROM `test-project-461721.cyclistic_bike_share_data.202410-divvy-tripdata`
+UNION ALL
+SELECT * FROM `test-project-461721.cyclistic_bike_share_data.202411-divvy-tripdata`
+UNION ALL
+SELECT * FROM `test-project-461721.cyclistic_bike_share_data.202412-divvy-tripdata`
+UNION ALL
+SELECT * FROM `test-project-461721.cyclistic_bike_share_data.202501-divvy-tripdata`
+UNION ALL
+SELECT * FROM `test-project-461721.cyclistic_bike_share_data.202502-divvy-tripdata`
+UNION ALL
+SELECT * FROM `test-project-461721.cyclistic_bike_share_data.202503-divvy-tripdata`
+UNION ALL
+SELECT * FROM `test-project-461721.cyclistic_bike_share_data.202504-divvy-tripdata`
+UNION ALL
+SELECT * FROM `test-project-461721.cyclistic_bike_share_data.202505-divvy-tripdata`
